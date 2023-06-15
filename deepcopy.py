78bbc59c797b7ab089fb0034393f629073dc2322
Vahag <135336961+Vahag5@users.noreply.github.com>
2023-06-15 13:11:27 +0400

    Add files via upload

---
+++ deepcopy.py
@@ -0,0 +1,18 @@
+ls2 = []
+def deepcopy(ls):
+    qan = len(ls)
+    ls2 = []
+    for i in range(qan): 
+        if type(ls[i]) == list:
+            ls2.append(ls[i].copy())     #karanq  senc el ls2[i] = ls[i].copy()
+            for j in range(len(ls[i])):
+                if type(ls[i][j]) == list:
+                    ls2[i][j] = ls[i][j].copy()
+        else:
+            ls2.append(ls[i])
+    
+    print(ls2)
+    return ls2
+
+ls = [1, 2, [4, 6], 'hello']
+deepcopy(ls)
